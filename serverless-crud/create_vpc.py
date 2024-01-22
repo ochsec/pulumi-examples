672a2fb112e4d9e56d5a9a6a27dd7a44e6a3752a
ochsec <cochsenreither@protonmail.com>
2024-01-21 17:03:50 -0700

    fix for subnets

--- serverless-crud/create_vpc.py
+++ serverless-crud/create_vpc.py
@@ -3,10 +3,18 @@ import pulumi_aws as aws
 def create_vpc():
     # Create the VPC for the Aurora serverless DB cluster
     vpc = aws.ec2.Vpc("vpc", cidr_block="10.0.0.0/16")
+    azs = aws.get_availability_zones()
 
     # Create subnets
-    subnet1 = aws.ec2.Subnet("subnet1", vpc_id=vpc.id, cidr_block="10.0.1.0/24")
-    subnet2 = aws.ec2.Subnet("subnet2", vpc_id=vpc.id, cidr_block="10.0.2.0/24")
+    subnet1 = aws.ec2.Subnet("subnet1", 
+                             vpc_id=vpc.id, 
+                             cidr_block="10.0.1.0/24",
+                             availability_zone=azs.names[0])
+    
+    subnet2 = aws.ec2.Subnet("subnet2",
+                             vpc_id=vpc.id,
+                             cidr_block="10.0.2.0/24",
+                             availability_zone=azs.names[1])
 
     db_subnet_group = aws.rds.SubnetGroup("db_subnet_group",
                                         subnet_ids=[subnet1.id, subnet2.id])
